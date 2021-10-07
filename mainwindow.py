c85a310410a439bb5809fd285074671783fe27ac
mjindriati <maharani.jelita@ui.ac.id>
2021-10-07 13:59:05 +0700

    masukkin gui login

--- mainwindow.py
+++ mainwindow.py
@@ -3,18 +3,18 @@ from PyQt5.uic import loadUi
 import sys
 import numpy as np
 import math
-import gambar_rc
+#import gambar_rc
 
-class mainwindow(QMainWindow):
+class LoginPage(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
-        #loadUi("sinusoid_gui.ui",self)
-        self.setWindowTitle("GUI PRAKTIKUM")
+        loadUi("Login_Page.ui",self)
+        self.setWindowTitle("Login Page")
         #self.tmbl_gambar.clicked.connect(self.tombol_gambar)
     
 
 app = QApplication([])
-mainwindow = mainwindow()
+mainwindow = LoginPage()
 app.setQuitOnLastWindowClosed(True)
 mainwindow.show()
 sys.exit(app.exec_())

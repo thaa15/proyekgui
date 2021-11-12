31356fbcf8405b994fb2f3887069ef11b997ce50
mjindriati <maharani.jelita@ui.ac.id>
2021-11-12 15:03:34 +0700

    modul 3

--- mainwindow.py
+++ mainwindow.py
@@ -130,6 +130,7 @@ class Modul_modulpage(QMainWindow):
         self.setWindowTitle("Pilih Modul")
         self.modul_1_button.clicked.connect(self.modul_1Page)
         self.modul_2_button.clicked.connect(self.modul_2Page)
+        self.modul_2_button.clicked.connect(self.modul_3Page)
         self.modul_6_button.clicked.connect(self.modul_6Page)
         self.logout_button.clicked.connect(self.logoutPage)
         self.nama_label.setText(f'Nama: {self.nama}')
@@ -149,6 +150,14 @@ class Modul_modulpage(QMainWindow):
         payload = {'activity' : 'Modul 2'}
         requests.put(f'https://gui-kel-1.herokuapp.com/profiles/{self.token}', data=payload)
         self.CurrentWindow = Modul_2(self.nama, self.npm, self.token)
+        self.force_close = False
+        self.CurrentWindow.show()
+        self.close()
+    
+    def modul_3Page(self):
+        payload = {'activity' : 'Modul 3'}
+        requests.put(f'https://gui-kel-1.herokuapp.com/profiles/{self.token}', data=payload)
+        self.CurrentWindow = Modul_3(self.nama, self.npm, self.token)
         self.force_close = False
         self.CurrentWindow.show()
         self.close()
@@ -400,6 +409,173 @@ class Modul_2(QMainWindow):
         ki = str(round(ki,2))
         self.ki.setText(ki)
 
+
+    def back_main_menu(self):
+        payload = {'activity' : 'Main Menu'}
+        requests.put(f'https://gui-kel-1.herokuapp.com/profiles/{self.token}', data=payload)
+        self.CurrentWindow = Modul_modulpage(self.nama, self.npm, self.token)
+        self.force_close = False
+        self.CurrentWindow.show()
+        self.close()
+
+    def closeEvent(self, event):
+        if self.force_close is True:
+            requests.delete(f'https://gui-kel-1.herokuapp.com/profiles/{self.token}')
+            sys.exit(0)
+
+class Modul_3(QMainWindow):
+    def __init__(self, nama, npm, token):
+        self.nama = nama
+        self.npm = npm
+        self.token = token
+        QMainWindow.__init__(self)
+        loadUi("Modul_3.ui", self)
+        self.setWindowTitle("Modul 3 - Saluran Transmisi")
+        self.calculate.clicked.connect(self.hitung)
+        self.menu.clicked.connect(self.back_main_menu)
+        app.aboutToQuit.connect(self.closeEvent)
+        self.force_close = True
+    
+    def hitung(self):
+        try: 
+        #ngambil data hubung singkat
+            global lokasiHS_1, lokasiHS_2, lokasiHS_3, lokasiHS_4, lokasiHS_5
+            global arusHS_1, arusHS_2, arusHS_3, arusHS_4, arusHS_5
+            lokasiHS_1 = float(self.lokasi_hs_1.text())
+            lokasiHS_2 = float(self.lokasi_hs_2.text())
+            lokasiHS_3 = float(self.lokasi_hs_3.text())
+            lokasiHS_4 = float(self.lokasi_hs_4.text())
+            lokasiHS_5 = float(self.lokasi_hs_5.text())
+            arusHS_1 = float(self.arus_hs_1.text())
+            arusHS_2 = float(self.arus_hs_2.text())
+            arusHS_3 = float(self.arus_hs_3.text())
+            arusHS_4 = float(self.arus_hs_4.text())
+            arusHS_5 = float(self.arus_hs_5.text())
+
+            #ngambil data resistif
+            global lokasiR_1, lokasiR_2, lokasiR_3, lokasiR_4, lokasiR_5
+            global arusR_1, arusR_2, arusR_3, arusR_4, arusR_5
+            lokasiR_1 = float(self.lokasi_r_1.text())
+            lokasiR_2 = float(self.lokasi_r_2.text())
+            lokasiR_3 = float(self.lokasi_r_3.text())
+            lokasiR_4 = float(self.lokasi_r_4.text())
+            lokasiR_5 = float(self.lokasi_r_5.text())
+            arusR_1 = float(self.arus_r_1.text())
+            arusR_2 = float(self.arus_r_2.text())
+            arusR_3 = float(self.arus_r_3.text())
+            arusR_4 = float(self.arus_r_4.text())
+            arusR_5 = float(self.arus_r_5.text())
+
+            #ngambil data antena
+            global lokasiA_1, lokasiA_2, lokasiA_3, lokasiA_4, lokasiA_5
+            global arusA_1, arusA_2, arusA_3, arusA_4, arusA_5
+            lokasiA_1 = float(self.lokasi_a_1.text())
+            lokasiA_2 = float(self.lokasi_a_2.text())
+            lokasiA_3 = float(self.lokasi_a_3.text())
+            lokasiA_4 = float(self.lokasi_a_4.text())
+            lokasiA_5 = float(self.lokasi_a_5.text())
+            arusA_1 = float(self.arus_a_1.text())
+            arusA_2 = float(self.arus_a_2.text())
+            arusA_3 = float(self.arus_a_3.text())
+            arusA_4 = float(self.arus_a_4.text())
+            arusA_5 = float(self.arus_a_5.text())
+
+            #ngambil data antena
+            global lokasiG_1, lokasiG_2, lokasiG_3, lokasiG_4, lokasiG_5
+            global arusG_1, arusG_2, arusG_3, arusG_4, arusG_5
+            lokasiG_1 = float(self.lokasi_g_1.text())
+            lokasiG_2 = float(self.lokasi_g_2.text())
+            lokasiG_3 = float(self.lokasi_g_3.text())
+            lokasiG_4 = float(self.lokasi_g_4.text())
+            lokasiG_5 = float(self.lokasi_g_5.text())
+            arusG_1 = float(self.arus_g_1.text())
+            arusG_2 = float(self.arus_g_2.text())
+            arusG_3 = float(self.arus_g_3.text())
+            arusG_4 = float(self.arus_g_4.text())
+            arusG_5 = float(self.arus_g_5.text())
+
+            #panjang gelombang pertama
+            lamda_13 = 2*(lokasiHS_3 - lokasiHS_1)
+            lamda_13 = str(round(lamda_13,2))
+            lamda_35 = 2*(lokasiHS_5 - lokasiHS_3)
+            lamda_35 = str(round(lamda_35,2))
+            lamda_rata = (float(lamda_13) + float(lamda_35))/2
+            lamda_rata = str(round(lamda_rata,2))
+            self.lamda13.setText(lamda_13)
+            self.lamda35.setText(lamda_35)
+            self.lamda_rata.setText(lamda_rata)
+
+            #nilai d beban
+            d_r = (lokasiR_1 - lokasiHS_1 + lokasiR_3 - lokasiHS_3 + lokasiR_5 - lokasiHS_5)/3
+            d_r = str(round(d_r, 2))
+            d_a = (lokasiA_1 - lokasiHS_1 + lokasiA_3 - lokasiHS_3 + lokasiA_5 - lokasiHS_5)/3
+            d_a = str(round(d_a, 2))
+            d_g = (lokasiG_1 - lokasiHS_1 + lokasiG_3 - lokasiHS_3 + lokasiG_5 - lokasiHS_5)/3
+            d_g = str(round(d_g, 2))
+            self.beban_r.setText(d_r)
+            self.beban_a.setText(d_a)
+            self.beban_g.setText(d_g)
+
+            #panjang gelombang
+            lamdaR = (float(d_r)/float(lamda_rata))
+            lamdaR = str(round(lamdaR, 2))
+            lamdaA = (float(d_a)/float(lamda_rata))
+            lamdaA = str(round(lamdaA, 2))
+            lamdaG = (float(d_g)/float(lamda_rata))
+            lamdaG = str(round(lamdaG, 2))
+            self.lamda_r.setText(lamdaR)
+            self.lamda_a.setText(lamdaA)
+            self.lamda_g.setText(lamdaG)
+
+            #vswr
+            vswr_r = math.sqrt(arusR_2/arusR_1)
+            vswr_r = str(round(vswr_r, 2))
+            vswr_a = math.sqrt(arusA_2/arusA_1)
+            vswr_a = str(round(vswr_a, 2))
+            vswr_g = math.sqrt(arusG_2/arusG_1)
+            vswr_g = str(round(vswr_g, 2))
+            self.vswr_r.setText(vswr_r)
+            self.vswr_a.setText(vswr_a)
+            self.vswr_g.setText(vswr_g)
+
+            #koefisien pantul
+            kp_R = (float(vswr_r) - 1)/(float(vswr_r) + 1)
+            kp_R = str(round(kp_R, 2))
+            kp_A = (float(vswr_a) - 1)/(float(vswr_a) + 1)
+            kp_A = str(round(kp_A, 2))
+            kp_G = (float(vswr_g) - 1)/(float(vswr_g) + 1)
+            kp_G = str(round(kp_G, 2))
+            self.koefpantul_r.setText(kp_R)
+            self.koefpantul_a.setText(kp_A)
+            self.koefpantul_g.setText(kp_G)
+
+            #return loss
+            rl_r = 20*math.log10((float(vswr_r) + 1)/(float(vswr_r) - 1))
+            rl_r = str(round(rl_r, 2))
+            rl_a = 20*math.log10((float(vswr_a) + 1)/(float(vswr_a) - 1))
+            rl_a = str(round(rl_a, 2))
+            rl_g = 20*math.log10((float(vswr_g) + 1)/(float(vswr_g) - 1))
+            rl_g = str(round(rl_g, 2))
+            self.rl_r.setText(rl_r)
+            self.rl_a.setText(rl_a)
+            self.rl_g.setText(rl_g)
+
+            #presentase daya yang dipantulkan
+            pr_r = (float(kp_R)**2)*100
+            pr_r = str(round(pr_r, 2))
+            persen_r = pr_r + "%"
+            pr_a = (float(kp_A)**2)*100
+            pr_a = str(round(pr_a, 2))
+            persen_a = pr_a + "%"
+            pr_g = (float(kp_G)**2)*100
+            pr_g = str(round(pr_g, 2))
+            persen_g = pr_g + "%"
+            self.pr_r.setText(persen_r)
+            self.pr_a.setText(persen_a)
+            self.pr_g.setText(persen_g)
+
+        except Exception:
+            QMessageBox.about(self, "Error", "Isi semua input!")
 
     def back_main_menu(self):
         payload = {'activity' : 'Main Menu'}
